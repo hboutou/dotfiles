8f7a8d4248b26a550611faf33155c68f5db3f68a
hboutou <22304469+hboutou@users.noreply.github.com>
2023-08-31 10:24:43 -0400

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 alias  ll='exa --group-directories-first --long'
 alias lll='exa --group-directories-first --long --all'
 
-set PIP_REQUIRE_VIRTUALENV 1
+set PIP_REQUIRE_VIRTUALENV true
 
 set --erase fish_greeting
 set PATH $HOME/.cargo/bin $PATH
