2369c72c9f78a8a34b8a472d4bd74641f035fd7b
hboutou <22304469+hboutou@users.noreply.github.com>
2024-03-24 14:01:46 -0400

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,9 @@ set -g fish_greeting
 
 set PATH $HOME/.cargo/bin $PATH
 
+set PYTHONDONTWRITEBYTECODE true
 set PIP_REQUIRE_VIRTUALENV true
+
 
 alias   l='eza --group-directories-first'                                       
 alias  ll='eza --group-directories-first --long'                                
