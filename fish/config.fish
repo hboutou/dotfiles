c8332a3a29b673df5de06fc6a3d4193d51c9305f
hboutou <22304469+hboutou@users.noreply.github.com>
2024-03-24 14:02:24 -0400

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,7 @@ set PYTHONDONTWRITEBYTECODE true
 set PIP_REQUIRE_VIRTUALENV true
 
 
+alias tree='eza --tree'
 alias   l='eza --group-directories-first'                                       
 alias  ll='eza --group-directories-first --long'                                
 alias lll='eza --group-directories-first --long --all'
-alias tree='eza --tree'
