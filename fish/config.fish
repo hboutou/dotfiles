ab5474edf30c75a574d51b8b34c9c3c8d4184846
hboutou <22304469+hboutou@users.noreply.github.com>
2024-07-13 13:23:47 -0400

    add config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -4,9 +4,9 @@ set PATH $HOME/.cargo/bin $PATH
 
 set PYTHONDONTWRITEBYTECODE true
 
-alias tree='eza --tree'
 alias   l='eza --group-directories-first'                                       
 alias  ll='eza --group-directories-first --long'                                
 alias lll='eza --group-directories-first --long --all'
+alias tree='eza --tree'
 
 alias sqlite="$(brew --prefix)/opt/sqlite3/bin/sqlite3"
