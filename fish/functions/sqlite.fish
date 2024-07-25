e3551bd5a2c078d3dd65e670b0fea08ea7be0160
hboutou <22304469+hboutou@users.noreply.github.com>
2024-07-25 16:32:04 -0400

    Update sqlite.fish

--- fish/functions/sqlite.fish
+++ fish/functions/sqlite.fish
@@ -1,3 +1,5 @@
 function sqlite
-  $(brew --prefix)/opt/sqlite3/bin/sqlite3 $argv
+    # $(brew --prefix)/opt/sqlite3/bin/sqlite3 $argv
+    # $(brew --prefix) makes shell startup time unbearable
+    /opt/homebrew/opt/sqlite3/bin/sqlite3 $argv
 end
