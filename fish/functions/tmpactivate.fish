4a92c97ae54658f7a0504ec6cdb253d309d6002b
hboutou <22304469+hboutou@users.noreply.github.com>
2024-07-25 16:42:57 -0400

    Update tmpactivate.fish

--- fish/functions/tmpactivate.fish
+++ fish/functions/tmpactivate.fish
@@ -1,3 +1,11 @@
 function tmpactivate
     source ~/tmppython/bin/activate.fish
+    : 'TODO
+    if /opt/tmppython and /opt/tmppython/bin/activate.fish exist
+        source /opt/tmppython/bin/activate.fish
+    else
+        python3.12 -m venv /opt/tmppython
+        /opt/tmppython/bin/pip install --upgrade pip
+        source /opt/tmppython/bin/activate.fish
+    '
 end
