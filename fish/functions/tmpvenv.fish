0340b389730687ce53fb168e9cd268acbc292981
hboutou <22304469+hboutou@users.noreply.github.com>
2024-09-10 16:48:51 -0400

    Update tmpvenv.fish

--- fish/functions/tmpvenv.fish
+++ fish/functions/tmpvenv.fish
@@ -3,29 +3,30 @@ function tmpvenv --description "Create a throwaway python playground"
 
     if test -f "$venvdir/bin/activate.fish"
         source "$venvdir/bin/activate.fish"
-    else
-        rm -rf "$venvdir"
-        python3.13 -m venv "$venvdir"
-        source "$venvdir/bin/activate.fish"
-        pip install --upgrade pip
-        pip -v install \
-            boto3 \
-            duckdb \
-            fastavro \
-            ipython \
-            matplotlib \
-            numpy \
-            pandas \
-            psycopg-binary \
-            pyarrow \
-            pydantic \
-            pydantic-settings \
-            # pymssql # needs a ceremony to compile
-            pyright \
-            redis \
-            requests \
-            scipy \
-            xwing \
+        return
+
+    rm -rf "$venvdir"
+    python3.13 -m venv "$venvdir"
+    source "$venvdir/bin/activate.fish"
+    pip install --upgrade pip
+    pip -v install \
+        boto3 \
+        duckdb \
+        fastavro \
+        ipython \
+        matplotlib \
+        numpy \
+        pandas \
+        psycopg-binary \
+        pyarrow \
+        pydantic \
+        pydantic-settings \
+        # pymssql # needs a ceremony to compile
+        pyright \
+        redis \
+        requests \
+        scipy \
+        xwing \
 
     end
 end
