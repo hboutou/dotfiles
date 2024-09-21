344c396bd7fee121fb89c3cc3c8bb5bb0fbea025
hboutou <22304469+hboutou@users.noreply.github.com>
2024-09-21 15:00:23 -0400

    sync

--- fish/functions/zscaler.fish
+++ fish/functions/zscaler.fish
@@ -4,5 +4,5 @@ function zscaler --description "notify when this crapware reconnects to internet
         sleep 5
     end
     tput bel
-    echo (date '+%F %T') - online
+    echo (date '+%F %T') - reconnected
 end
