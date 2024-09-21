01bf4c282210ca3b2f59b46e0e2f9395c43d70ae
hboutou <22304469+hboutou@users.noreply.github.com>
2024-09-21 15:03:07 -0400

    Update zscaler.fish

--- fish/functions/zscaler.fish
+++ fish/functions/zscaler.fish
@@ -1,8 +1,9 @@
 function zscaler --description "notify when this crapware reconnects to internet"
     echo (date '+%F %T') - start
     while not ping -c 1 google.com
+        echo (date '+%F %T') - offline
         sleep 5
     end
     tput bel
-    echo (date '+%F %T') - reconnected
+    echo (date '+%F %T') - online
 end
