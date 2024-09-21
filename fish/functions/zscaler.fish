39043b0fe0e3252e151e7ff620f530147f812021
hboutou <22304469+hboutou@users.noreply.github.com>
2024-09-21 14:49:36 -0400

    sync

--- fish/functions/zscaler.fish
+++ fish/functions/zscaler.fish
@@ -1,6 +1,7 @@
-function zscaler --description "notify when this crapware reconnects me to internet"                                               
-    while not ping -c 1 google.com                                              
-        sleep 1                                                                 
-    end                                                                         
-    tput bel                                                                    
+function zscaler --description "notify when this crapware reconnects me to internet"
+    while not ping -c 1 google.com
+        sleep 1
+    end
+    echo (date '+%F %T') - online
+    tput bel
 end
