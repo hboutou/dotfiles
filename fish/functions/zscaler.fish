829ca19bb16ee0fb6fc03b768bc908d1b6f2b833
hboutou <22304469+hboutou@users.noreply.github.com>
2024-09-30 02:46:10 -0400

    sync

--- fish/functions/zscaler.fish
+++ fish/functions/zscaler.fish
@@ -1,7 +1,7 @@
 function zscaler --description "notify when this crapware reconnects to internet"
     echo (date '+%F %T') - start
-    while not ping -c 1 1.1.1.1
-    end
+    while not ping -c 1 1.1.1.1; end
     tput bel
     echo (date '+%F %T') - online
+    python3 -c "from datetime import timedelta; print('offline for', timedelta(milliseconds=$CMD_DURATION), 'thanks zscaler')"
 end
