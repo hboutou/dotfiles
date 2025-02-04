fd8b0afce5041891134b7363a20f7a794250246b
hboutou <22304469+hboutou@users.noreply.github.com>
2025-02-04 04:50:51 -0500

    sync

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,5 @@
+fish_add_path $HOME/scripts
+
 set -g fish_greeting
 
 set PATH $HOME/.cargo/bin $PATH
@@ -9,6 +11,8 @@ set PYTHONDONTWRITEBYTECODE 1
 set DOTNET_CLI_TELEMETRY_OPTOUT 1
 
 set -Ux LESS -R -S -F
+
+
 
 # integrate aws_completer see: https://github.com/aws/aws-cli/issues/1079
 test -x (which aws_completer); and complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
