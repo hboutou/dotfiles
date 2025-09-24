7a4650a730b020cabddb97357eca0a78f0219371
hboutou <22304469+hboutou@users.noreply.github.com>
2025-09-24 12:55:53 -0400

    sync

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,9 @@
 fish_add_path $HOME/scripts
 fish_add_path $HOME/go/bin
+fish_add_path /opt/homebrew/bin
 
 set -g fish_greeting ""
-set fish_color_valid_path
+set -g fish_color_valid_path ""
 
 set PYTHONDONTWRITEBYTECODE 1
 set DOTNET_CLI_TELEMETRY_OPTOUT 1
@@ -11,7 +12,14 @@ set AWS_JAVA_V1_DISABLE_DEPRECATION_ANNOUNCEMENT true
 set -Ux LESS -R -S -F
 
 if status is-interactive
+    set CACHE_BD     ""
+    set CACHE_BD_PRD ""
+    set CACHE_TS     ""
+    set CACHE_TS_PRD ""
+
+    alias poetry1.1.15 "uv tool run poetry@1.1.15"
+
 	# integrate aws_completer see: https://github.com/aws/aws-cli/issues/1079
-	test -x (which aws_completer); and complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
+    # test -x (which aws_completer); and complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
 end
 
